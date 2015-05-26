336fcbe0360ec18b5cb2135e009a62939d8b476b
Kevin <kaiwenbc@gmail.com>
2015-05-26 13:42:18 -0700

    fixed email address

--- x11/hacks/glx/eight.c
+++ x11/hacks/glx/eight.c
@@ -1,4 +1,4 @@
-/* xscreensaver, Copyright (c) 1998-2015 Kevin Zhang <sudo@pt-get.org>
+/* xscreensaver, Copyright (c) 1998-2015 Kevin Zhang <sudo@pt-get.com>
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
